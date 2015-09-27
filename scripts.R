9cefad2b9208902a354e48e6fa9b1b1ea5882238
yaroslav.yermilov <yermilov17@gmail.com>
2015-09-27 22:40:30 +0300

    analyze questions script

--- scripts.R
+++ scripts.R
@@ -5,7 +5,6 @@ set.seed(172843)
 
 # read data
 data <- read.table("data\\pml-training.csv", header = TRUE, sep = ",", colClasses = c( rep("character", 6), rep("numeric", 5), rep("character", 6), rep("numeric", 2), rep("character", 1), rep("numeric", 2), rep("character", 1), rep("numeric", 2), rep("character", 1), rep("numeric", 42), rep("character", 6), rep("numeric", 12), rep("character", 6), rep("numeric", 2), rep("character", 1), rep("numeric", 2), rep("character", 1), rep("numeric", 2), rep("character", 1), rep("numeric", 23), rep("character", 6), rep("numeric", 2), rep("character", 1), rep("numeric", 2), rep("character", 1), rep("numeric", 2), rep("character", 1), rep("numeric", 19), rep("character", 1) ))
-question_data <- read.table("data\\pml-testing.csv", header = TRUE, sep = ",", colClasses = c( rep("character", 6), rep("numeric", 5), rep("character", 6), rep("numeric", 2), rep("character", 1), rep("numeric", 2), rep("character", 1), rep("numeric", 2), rep("character", 1), rep("numeric", 42), rep("character", 6), rep("numeric", 12), rep("character", 6), rep("numeric", 2), rep("character", 1), rep("numeric", 2), rep("character", 1), rep("numeric", 2), rep("character", 1), rep("numeric", 23), rep("character", 6), rep("numeric", 2), rep("character", 1), rep("numeric", 2), rep("character", 1), rep("numeric", 2), rep("character", 1), rep("numeric", 19), rep("character", 1) ))
 
 # select only numeric columns
 data_columns <- sapply(data, is.numeric)
@@ -13,13 +12,13 @@ data_columns['classe'] = TRUE
 data <- data[data_columns]
 data$classe <- as.factor(data$classe)
 
-question_data <- question_data[data_columns]
-
 # data slicing
 train_data_index <- createDataPartition(y=data$classe, p = 0.7, list=FALSE)
 train_data <- data[train_data_index,]
+test_data <- data[-train_data_index,]
 
 train_data_index <- createDataPartition(y=train_data$classe, p = 0.7, list=FALSE)
+validation_data <- train_data[-train_data_index,]
 train_data <- train_data[train_data_index,]
 
 # preprocessing
@@ -31,19 +30,39 @@ imputed_data <- predict(impute_action, preprocess_data)
 pca_action <- preProcess(imputed_data, method="pca")
 pca_data <- predict(pca_action, imputed_data)
 
+# boosting with trees model
+model_gbm <- train(train_data$classe ~ ., method="gbm", data = pca_data)
+
 # random forest model
 model_rf <- train(train_data$classe ~ ., method="rf", data = pca_data, prox=TRUE)
 
-# preprocessing question data
-imputed_question_data <- predict(impute_action, question_data)
-pca_question_data <- predict(pca_action, imputed_question_data)
+# preprocessing validation data
+preprocess_validation_data <- validation_data[,-120]
+imputed_validation_data <- predict(preProcess(preprocess_data, method="knnImpute"), preprocess_validation_data)
+pca_validation_data <- predict(preProcess(imputed_data, method="pca"), imputed_validation_data)
 
-# find answers
-answers <- predict(model_rf, pca_question_data)
+# random forest validation
+predictions_rf <- predict(model_rf, pca_validation_data)
+confusionMatrix(predictions_rf, validation_data$classe)
 
-# write answers to file
-for(i in 1:length(answers)) {
-    filename = paste0("answers\\problem_id_", i, ".txt")
-    write.table(answers[i], file=filename, quote=FALSE, row.names=FALSE, col.names=FALSE)
-  }
-}
+# boosting with trees validation
+predictions_gbm <- predict(model_gbm, pca_validation_data)
+confusionMatrix(predictions_gbm, validation_data$classe)
+
+# preprocessing test data
+preprocess_test_data <- test_data[,-120]
+imputed_test_data <- predict(preProcess(preprocess_data, method="knnImpute"), preprocess_test_data)
+pca_test_data <- predict(preProcess(imputed_data, method="pca"), imputed_test_data)
+
+# random forest test
+test_rf <- predict(model_rf, pca_test_data)
+confusionMatrix(test_rf, test_data$classe)
+
+# boosting with trees test
+test_gbm <- predict(model_gbm, pca_test_data)
+confusionMatrix(test_gbm, test_data$classe)
+
+
+# report
+library(knitr)
+knit2html("report.Rmd")
